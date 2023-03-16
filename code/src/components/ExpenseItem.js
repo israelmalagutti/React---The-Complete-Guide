a8727660b22a7bfc61362cf5b8e8112c49dfbe7f
Israel Malagutti <israelmalagutti@gmail.com>
2023-03-16 13:53:17 -0300

    04-adding-basic-css-styling

--- code/src/components/ExpenseItem.js
+++ code/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
-import React from "react";
+import "./ExpenseItem.css";
 
-const ExpenseItem = ({ date, expenseType, value }) => {
+const ExpenseItem = ({ date, expenseType, price }) => {
   return (
-    <div>
+    <div className="expense-item">
       <div>{date || "March 28th 2021"}</div>
-      <div>
+
+      <div className="expense-item__description">
         <h2>{expenseType || "Car Insurance"}</h2>
-        <div>${value || 297.67}</div>
+        <div className="expense-item__price">${price || 297.67}</div>
       </div>
     </div>
   );
