dfd759ea7db4f846b9692b2b3179263d2b52e848
Israel Malagutti <israelmalagutti@gmail.com>
2023-03-16 13:47:05 -0300

    chore: change directory name

---
+++ code/src/App.js
@@ -0,0 +1,12 @@
+import { ExpenseItem } from "./components";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started y'all!!</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
--- react-complete-guide/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h2>Let's get started y'all!!</h2>
-    </div>
-  );
-}
-
-export default App;
