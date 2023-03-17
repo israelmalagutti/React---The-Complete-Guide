88aa452d7fcaab034bc9fd1d743dabe1923a22cc
Israel Malagutti <israelmalagutti@gmail.com>
2023-03-17 12:53:05 -0300

    10-the-concept-of-composition

--- code/src/components/ExpenseItem.js
+++ code/src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import "./ExpenseItem.css";
 
+import { Card } from "./Card";
 import { ExpenseDate } from "./ExpenseDate";
 
 const ExpenseItem = ({ date, title, price }) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
 
       <div className="expense-item__description">
         <h2>{title || "Car Insurance"}</h2>
         <div className="expense-item__price">${price || 297.67}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- code/src/components/index.js
+++ code/src/components/index.js
@@ -1,3 +1,4 @@
 // export * from '';
+export * from "./Card.js";
 export * from "./ExpenseItem.js";
 export * from "./Expenses.js";
