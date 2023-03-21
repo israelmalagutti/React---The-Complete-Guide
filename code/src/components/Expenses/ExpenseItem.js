9738e112143d860d86161bca9daddd5499136297
Israel Malagutti <israelmalagutti@gmail.com>
2023-03-21 13:38:36 -0300

    01-working-with-state

--- code/src/components/Expenses/ExpenseItem.js
+++ code/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 
 import { Card } from "../Ui";
 import { ExpenseDate } from "./ExpenseDate";
 
 const ExpenseItem = ({ date, title, price }) => {
+  const [expenseTitle, setExpenseTitle] = useState(title);
+
+  const clickHandler = () => {
+    setExpenseTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
 
       <div className="expense-item__description">
-        <h2>{title || "Car Insurance"}</h2>
+        <h2>{expenseTitle || "Car Insurance"}</h2>
         <div className="expense-item__price">${price || 297.67}</div>
       </div>
+      <button onClick={clickHandler}>Change expense title</button>
     </Card>
   );
 };
