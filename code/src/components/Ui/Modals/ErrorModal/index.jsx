416188039e77b3587955c42ebd99168d494ee1c8
Israel Malagutti <israelmalagutti@gmail.com>
2023-03-27 17:15:48 -0300

    04-working-with-portals

--- code/src/components/Ui/Modals/ErrorModal/index.jsx
+++ code/src/components/Ui/Modals/ErrorModal/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { Button } from "../../Button";
 
 import { Actions, Backdrop, Content, Header, StyledCard } from "./styles";
@@ -6,27 +7,40 @@ import { Actions, Backdrop, Content, Header, StyledCard } from "./styles";
 const STANDARD_ERROR_TITLE = "Invalid input";
 const STANDARD_ERROR_MESSAGE = "An error has occoured, please try again later";
 
-export const ErrorModal = ({
+const ModalOverLay = ({
   title = STANDARD_ERROR_TITLE,
   message = STANDARD_ERROR_MESSAGE,
   onConfirm,
 }) => {
   return (
+    <StyledCard>
+      <Header>
+        <h2>{title}</h2>
+      </Header>
+
+      <Content>
+        <p>{message}</p>
+      </Content>
+
+      <Actions>
+        <Button onClick={onConfirm}>Okay</Button>
+      </Actions>
+    </StyledCard>
+  );
+};
+
+export const ErrorModal = ({ title, message, onConfirm }) => {
+  return (
     <div>
-      <Backdrop onClick={onConfirm} />
-      <StyledCard>
-        <Header>
-          <h2>{title}</h2>
-        </Header>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
 
-        <Content>
-          <p>{message}</p>
-        </Content>
-
-        <Actions>
-          <Button onClick={onConfirm}>Okay</Button>
-        </Actions>
-      </StyledCard>
+      {ReactDOM.createPortal(
+        <ModalOverLay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById("overlay-root")
+      )}
     </div>
   );
 };
