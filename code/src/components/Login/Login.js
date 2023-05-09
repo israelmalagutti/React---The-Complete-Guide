710ff9a98c0c93519872e3c5076bf8cc7bf8c731
Israel Malagutti <israelmalagutti@gmail.com>
2023-05-09 14:42:26 -0300

    05-useeffect-summary

--- code/src/components/Login/Login.js
+++ code/src/components/Login/Login.js
@@ -41,9 +41,16 @@ const Login = (props) => {
   };
 
   useEffect(() => {
+    console.log("EFFECT RUNNING");
+
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Checking form validity!");
-
       setFormIsValid(
         enteredEmail.includes("@") && enteredPassword.trim().length > 6
       );
